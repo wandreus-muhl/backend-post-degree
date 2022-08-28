d5240db008e360b807f62c6b4d6a7137fa4ee72b
Wandreus <wandreusmuhl70@gmail.com>
2022-08-28 12:06:16 -0400

    feature: env for typeORM

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-const express = require('express')
+import 'reflect-metadata'
+import 'dotenv/config'
+import express from 'express'
 
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Tá on ihaaaaaaaaaaaaaaaa!!!!!!!!!!'))
+app.get('/', (req, res) => res.send('Tá on ihaaa!!!'))
 app.get('/:number', (req, res) => res.send(`You asked for number ${req.params.number}`))
 
-const port = 3000
+const port = process.env.API_PORT || 3000
 app.listen(port, () => console.log(`Server is running! http://localhost:${port}`))
